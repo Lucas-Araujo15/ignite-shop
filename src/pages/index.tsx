5758e66e18f698fa1263cacf15986b6df81269bf
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-05-09 16:42:12 -0300

    Estrutura visual - Estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { styled } from '../styles'
 
 // Estilização aplicada somente em Runtime (Tempo de execução)
 const Button = styled('button', {
-  backgroundColor: '$rocketseat',
+  backgroundColor: '$green300',
   borderRadius: 4,
   border: 0,
   padding: '4px 8px',
