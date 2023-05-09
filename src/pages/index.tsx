af8b919c8805a01b2e184457ba030a607f93c6f6
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-05-09 16:02:51 -0300

    Estrutura visual - Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
+import { styled } from '../styles'
+
+// Estilização aplicada somente em Runtime (Tempo de execução)
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+})
+
 export default function Home() {
-  return <h1>Hello World!</h1>
+  return (
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
+  )
 }
