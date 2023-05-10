563dc3e2d27bf2d241d4f98c1d1aa5a764931ecc
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-05-10 12:39:18 -0300

    Integração com Stripe - Configurando conta Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { useKeenSlider } from 'keen-slider/react'
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
 
 import 'keen-slider/keen-slider.min.css'
 
@@ -41,7 +42,7 @@ export default function Home() {
         </footer>
       </Product>
       <Product className="keen-slider__slide">
-        <Image src={camiseta3} width={520} height={480} alt="" />
+        <Image src={camiseta4} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta Z</strong>
           <span>R$ 79,90</span>
